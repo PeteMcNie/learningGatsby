b988dd450c5d2cecf719d720c1adc3621f5b20f4
Pete McNie <62854343+PeteMcNie@users.noreply.github.com>
2020-07-07 10:53:52 +1200

    Initial set up and Step 1 of getting to know gatsby

---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Header from '../pages/components/header'
+
+export default function Contact() {
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Header headerText="Contact" />
+      <p>Send us a message!</p>
+    </div>
+  )
+}
